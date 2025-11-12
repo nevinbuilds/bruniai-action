a4de2fb1ca3b384854e715a2befccd6155e5a2f5
joaogarin <joao.garin@gmail.com>
2025-11-12 16:04:53 +0000

    run vitest tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,5 +5,16 @@ export default defineConfig({
     globals: true,
     environment: "node",
     include: ["tests/**/*.test.ts"],
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "json", "html"],
+      exclude: [
+        "node_modules/",
+        "tests/",
+        "dist/",
+        "**/*.test.ts",
+        "**/*.config.ts",
+      ],
+    },
   },
 });
